6320cf589ece9f6eff90fb99e7547ebb175b632b
eschirni <eschirni@42heilbronn.de>
2022-01-14 20:18:28 +0100

    start of coding, create a branch and put it in executer

--- src/main.c
+++ src/main.c
@@ -6,19 +6,11 @@
 /*   By: eschirni <eschirni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/08 20:31:18 by eschirni          #+#    #+#             */
-/*   Updated: 2022/01/14 18:09:04 by eschirni         ###   ########.fr       */
+/*   Updated: 2022/01/14 20:18:22 by eschirni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-static void	signal_handler(int signal)
-{
-	if (signal == 2)
-		write(1, "\n", 1);
-	else if(signal == 3)
-		return ;
-}
 
 int	main(void)
 {
@@ -26,10 +18,8 @@ int	main(void)
 
 	while (1)
 	{
-		signal(SIGINT, signal_handler);
-		signal(SIGQUIT, signal_handler);
 		line = readline("minishell$ ");
-		//parser, executer, etc
+		//call your function here
 		free(line);
 	}
 }
