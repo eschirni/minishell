ff778d840fce41fab03b5b92f4433ae77e33b865
eschirni <eschirni@42heilbronn.de>
2022-02-01 20:26:54 +0100

    fixed export

--- src/executer/export.c
+++ src/executer/export.c
@@ -6,7 +6,7 @@
 /*   By: eschirni <eschirni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/25 17:58:13 by tom               #+#    #+#             */
-/*   Updated: 2022/01/31 18:09:52 by eschirni         ###   ########.fr       */
+/*   Updated: 2022/02/01 20:25:14 by eschirni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,57 +31,68 @@ static void	export_env(t_env **env, char *name)
 	tmp->export = true;
 }
 
-static void	sort_env(t_env *env_v)
-{
-	t_env	*current;
-	t_env	*index;
-	char	*name_tmp;
-	char	*value_tmp;
-
-	current = env_v;
-	index = NULL;
-	while (current != NULL)
-	{
-		index = current->next;
-		while (index != NULL)
-		{
-			if (ft_strcmp(current->name, index->name) > 0)
-			{
-				name_tmp = current->name;
-				value_tmp = current->value;
-				current->name = index->name;
-				current->value = index->value;
-				index->name = name_tmp;
-				index->value = value_tmp;
-			}
-			index = index->next;
-		}
-		current = current->next;
-	}
-}
-
-static void	print_export(t_env env_v)
+static t_env	*biggest_name(t_env *env_v)
 {
 	t_env	*tmp;
+	t_env	*ret;
 
-	tmp = &env_v;
-	sort_env(tmp);
+	ret = NULL;
+	tmp = env_v;
 	while (tmp != NULL)
 	{
-		if (tmp->export == true)
-			printf("declare -x %s=\"%s\"\n", tmp->name, tmp->value);
+		if (ret == NULL)
+			ret = tmp;
+		else if (strcmp(tmp->name, ret->name) > 0)
+			ret = tmp;
+		tmp = tmp->next;
+	}
+	return (ret);
+}
+
+static char	*sort_env(t_env *env_v, char *last)
+{
+	t_env	*tmp;
+	t_env	*low;
+	int		i;
+
+	tmp = env_v;
+	low = biggest_name(env_v);
+	while (tmp != NULL)
+	{
+		if (strcmp(low->name, tmp->name) > 0)
+		{
+			if (last == NULL || strcmp(tmp->name, last) > 0)
+				low = tmp;
+		}
+		tmp = tmp->next;
+	}
+	if (low->export == true)
+		printf("declare -x %s=\"%s\"\n", low->name, low->value);
+	return (low->name);
+}
+
+static void	print_export(t_env *env_v)
+{
+	t_env	*tmp;
+	char	*last;
+
+	last = NULL;
+	tmp = env_v;
+	while (tmp != NULL)
+	{
+		last = sort_env(env_v, last);
 		tmp = tmp->next;
 	}
 }
 
-void	export(t_env **env_v, char *arg)
+void	export(t_env *env_v, char *arg)
 {
 	int		pos;
 	char	*name;
 	char	*value;
 
 	if (arg == NULL)
-		print_export(**env_v);
+		print_export(env_v);
 	else
 	{
 		pos = ft_strclen(arg, '=');
@@ -90,11 +101,11 @@ void	export(t_env **env_v, char *arg)
 			name = ft_strndup(arg, pos);
 			value = ft_strcdup(arg, '\0', pos + 1);
 		}
-		if (pos == -1 && search_env(*env_v, name) == true)
-			export_env(env_v, arg);
-		else if (pos >= 0 && search_env(*env_v, name) == false)
-			add_env(env_v, name, value, true);
-		else if (pos >= 0 && search_env(*env_v, name) == true)
-			rep_env(env_v, name, value, true);
+		if (pos == -1 && search_env(env_v, name) == true)
+			export_env(&env_v, arg);
+		else if (pos >= 0 && search_env(env_v, name) == false)
+			add_env(&env_v, name, value, true);
+		else if (pos >= 0 && search_env(env_v, name) == true)
+			rep_env(&env_v, name, value, true);
 	}
 }
