a0843ac18094c521ef368f5b9534af2b163ca4b5
tom <tom@t-m1.local>
2022-02-26 17:45:34 +0100

    removed trash files and write() line

--- src/parser/pipe.c
+++ src/parser/pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipe.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eschirni <eschirni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tom <tom@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 16:20:43 by tom               #+#    #+#             */
-/*   Updated: 2022/02/26 16:08:48 by eschirni         ###   ########.fr       */
+/*   Updated: 2022/02/26 17:45:03 by tom              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,7 +102,6 @@ void	ft_pipe(t_token *tokens, char **envp, t_env *env_v)
 		pid = fork();
 		if (pid == 0)
 		{
-			write(1, "", 0);
 			if (input[i + 1] == NULL)
 				pipe_last(input[i], fd, envp, env_v, &tmp);
 			else
